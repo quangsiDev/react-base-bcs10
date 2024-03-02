f15d5144032948f0d8e391e72cc7554ca7355fc5
quangsiDev <tranquangsidev@gmail.com>
2024-03-02 11:47:03 +0700

    update

--- src/DemoProps/Card.jsx
+++ src/DemoProps/Card.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 
-export default class Card extends Component {
-  render() {
-    // this.props ~ nhận data từ component cha truyền vào dưới dạng object
-    console.log(this.props);
-    return (
-      <div className="card text-left">
-        <img className="card-img-top w-25" src={this.props.imgSrc} alt />
-        <div className="card-body">
-          <h4 className="card-title">{this.props.titleCard}</h4>
-          <p className="display-2">{this.props.username}</p>
-          <button onClick={this.props.handleClick} className="btn-warning">
-            Change name
-          </button>
-        </div>
+export default function Card({ imgSrc, titleCard, username, handleClick }) {
+  // render() {
+  // this.props ~ nhận data từ component cha truyền vào dưới dạng object
+  return (
+    <div className="card text-left">
+      <img className="card-img-top w-25" src={imgSrc} alt />
+      <div className="card-body">
+        <h4 className="card-title">{titleCard}</h4>
+        <p className="display-2">{username}</p>
+        <button onClick={handleClick} className="btn-warning">
+          Change name
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 // state ở đâu, setState tại đó
